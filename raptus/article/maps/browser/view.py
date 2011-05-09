37a029a45a1ddf376eb752f7dc5038909688bc13
Simon Käser <skaeser@raptus.com>
2011-05-09 09:18:47 +0000

    fixed javascript registry expressions and helper view to prevent Unauthorized exception

--- raptus/article/maps/browser/view.py
+++ raptus/article/maps/browser/view.py
@@ -1,28 +1,19 @@
 from Acquisition import aq_inner
 
-from AccessControl import ClassSecurityInfo
-
-from zope.component import queryAdapter
+from zope.interface import Interface, implements
 
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.statusmessages.interfaces import IStatusMessage
-from Products.CMFCore import permissions
 
 from plone.memoize.instance import memoize
 from raptus.article.core import RaptusArticleMessageFactory as _
 from raptus.article.core import interfaces
-from raptus.article.maps.interfaces import IMarkers
-from raptus.article.maps.interfaces import IMap, IMaps
+from raptus.article.maps.interfaces import IMarkers, IMap, IMaps, IMarker
 from raptus.article.maps.browser.maps import IMapsFull, IMapsLeft, IMapsRight
-from raptus.article.maps.interfaces import IMarker
-
-
 
 class View(BrowserView):
     """Map view
     """
-
     template = ViewPageTemplateFile('view.pt')
     
     def __call__(self):
@@ -43,13 +34,23 @@ class View(BrowserView):
                          'latitude': item['obj'].getLatitude()})
         return items
 
+class IHelperView(Interface):
+    """ Helper view
+    """
+    
+    def hasMaps():
+        """ Whether a map is available or not
+        """
 
 class HelperView(BrowserView):
     """ Used for a python expression in the javascript registry. its
         turn off google map api if it not used.
     """
+    implements(IHelperView)
     
     def hasMaps(self):
+        """ Whether a map is available or not
+        """
         return (
             IMap.providedBy(self.context) or
             IMarker.providedBy(self.context) or
@@ -57,9 +58,3 @@ class HelperView(BrowserView):
             IMapsLeft.providedBy(self.context) or 
             IMapsRight.providedBy(self.context) or
             IMaps.providedBy(self.context))
-
-    
-    
-    
-    
-    
